5cb8c49ee90522170765c7fc70b367f9916269d7
pdroxzxxz <ifpisul0@gmail.com>
2026-04-22 08:21:10 -0300

    0111

---
+++ atv 22/4/2026/atv1.py
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <h1>formularios</h1> 
+
+   <form>
+    LOGIN:<BR>
+    <input type="text" name="login"><BR>
+    
+        SENHA:<BR>
+    <input type="password" name="senha"><BR>
+    CONFIRMAR SENHA : <br>
+    <input type="password" name="confirmar_senha"><br>
+
+    nome ; <br>
+    <input type="text" name="nome"><br>
+
+    sexo : <br>
+    <input type="radio" value="masculino" name="sexo">masculino<br>
+    <input type="radio" value="feminino" name="sexo">feminino<br>
+
+    email : <br>
+    <input type="email" name="email"><br><br>
+
+    interesses : <br>
+    <input type="checkbox" name="futebol" value="interesses">futebol<br>
+    <input type="checkbox" name="politica" value="interesses">politica<br>
+    <input type="checkbox" name="Economia" value="interesses">Economia<br>
+    <input type="checkbox" name="carros" value="interesses">carros<br>
+    <input type="checkbox" name="celebridades" value="interesses">celebridades<br>
+
+    <input type="submit" value="enviar">
+   
+</form>
+
+
+
+
+
+
+
+
+</body>
+</html>
